ae0208a4bf24b303cd7987ec77484a7623f47287
Maruf520 <md.maruf5201@gmail.com>
2023-10-02 16:54:54 +0600

    command service added

---
+++ CommandService/Profiles/CommandsProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CommandService.Dtos;
+using CommandService.Models;
+using PlatformService;
+
+namespace CommandService.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Platform, PlatformreadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<PlatformPublishedDto, Platform>()
+                .ForMember(dest => dest.ExternalId, 
+                    act => act.MapFrom(src => src.Id));
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(dest => dest.ExternalId,
+                    opt => opt.MapFrom(src => src.PlatformId))
+                .ForMember(dest => dest.Name,
+                    opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands,
+                    opt => opt.Ignore());
+        }
+    }
+}
